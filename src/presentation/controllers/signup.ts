da636c88b2b178bcb0a48aa4d28f77f39dc6f8b3
Stefano <oliveira.stefano@gmail.com>
2021-02-22 14:28:34 -0300

    chore: bkp

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -7,10 +7,13 @@ import { InvalidParamError } from "../errors/invalid-param-error"
 
 
 export class SignUpController implements Controller{
+
     private readonly emailValidator: EmailValidator
+
     constructor(emailValidator: EmailValidator){
         this.emailValidator = emailValidator
     }
+    
     handle(httpRequest: HttpRequest): HttpResponse{
         const requireFields = ['name','email','password','passwordConfirmation']
         for (const field of requireFields){
