69841c858e3df7ea8a2563bdf21d4c6ba52b9f53
Stefano Oliveira <oliveira.stefano@gmail.com>
2021-03-29 10:32:29 -0300

    refactor: group all protocols in a single file

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,4 +1,4 @@
-import { Encrypter } from "../../protocols/encrypter"
+import { Encrypter } from "./db-add-account-protocols"
 import {DbAddAccount} from "./db-add-account"
 
 interface SutTypes{
@@ -50,5 +50,5 @@ describe('DbAddAccount Usecase', () => {
         await expect(promise).rejects.toThrow()
     })
 
-
+    
 })
